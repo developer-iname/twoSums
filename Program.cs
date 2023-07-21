13047110d4327356eec4f06ddd49045e947d9970
Ross S <developer@iname.com>
2023-07-21 17:56:04 +0100

    Passed test cases. Sub failed = 0 + nonSortedArr

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] numbers = {2,7,11,15};
+int target = 9;
+
+foreach(int number in TwoSum(numbers,target))
+{
+    Console.WriteLine(number);
+}
+
+int[] TwoSum(int[] nums, int target) {
+    int sumReturned = 0;
+    for(int x = 0; x < nums.Length; x++)
+    {
+        if(nums[x] > target)
+        {
+            break;
+        }
+        else
+        {
+            for(int i = x + 1; i < nums.Length; i++)
+            {
+                if(nums[i] > target)
+                {
+                    break;
+                }
+                sumReturned = nums[x] + nums[i];
+                if(sumReturned == target)
+                {
+                    return new int[] {x,i};
+                }
+            }
+        }
+    }
+    return new int[0];
+}
